423b4f191b4cf12c6fb1ddb8b44cbe5c7886908c
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2023-02-05 16:55:26 -0400

    + add DataGrid

--- src/pages/workItemsPage/WorkItemsPage.tsx
+++ src/pages/workItemsPage/WorkItemsPage.tsx
@@ -1,7 +1,8 @@
 import React, { useState, MouseEvent } from 'react'
-import { Box, Typography, Stack, MenuProps, styled, Menu, Button, MenuItem, alpha, Checkbox, Divider } from '@mui/material'
+import { Box, Typography, Stack, MenuProps, styled, Menu, Button, MenuItem, alpha, Checkbox, Divider, Theme } from '@mui/material'
 import { AssignmentTurnedIn, BakeryDining, EmojiEvents, FilterAltOutlined, KeyboardArrowDown, ListAlt, Park, PestControl, Science, Clear } from '@mui/icons-material'
 import { amber, blue, deepOrange, deepPurple, grey, purple, red, teal } from '@mui/material/colors'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import { ListOptionsBar } from '../../components'
 
 const initialItemTypes = [
@@ -63,6 +64,98 @@ const initialItemTypes = [
   }
 ]
 
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      `${params.row.firstName ?? ''} ${params.row.lastName ?? ''}`
+  }
+]
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
+]
+
+function customCheckbox (theme: Theme): any {
+  return {
+    '& .MuiCheckbox-root svg': {
+      width: 16,
+      height: 16,
+      backgroundColor: 'transparent',
+      border: `2px solid ${
+        theme.palette.mode === 'light' ? grey[400] : 'rgb(67, 67, 67)'
+      }`,
+      borderRadius: '50%'
+    },
+    '& .MuiCheckbox-root svg path': {
+      display: 'none'
+    },
+    '& .MuiCheckbox-root.Mui-checked:not(.MuiCheckbox-indeterminate) svg': {
+      backgroundColor: '#1890ff',
+      borderColor: '#1890ff'
+    },
+    '& .MuiCheckbox-root.Mui-checked .MuiIconButton-label:after': {
+      position: 'absolute',
+      display: 'table',
+      border: '2px solid #fff',
+      borderTop: 0,
+      borderLeft: 0,
+      transform: 'rotate(45deg) translate(-50%,-50%)',
+      opacity: 1,
+      transition: 'all .2s cubic-bezier(.12,.4,.29,1.46) .1s',
+      content: '""',
+      top: '50%',
+      left: '39%',
+      width: 5.71428571,
+      height: 9.14285714
+    },
+    '& .MuiCheckbox-root.MuiCheckbox-indeterminate .MuiIconButton-label:after': {
+      width: 8,
+      height: 8,
+      backgroundColor: '#1890ff',
+      transform: 'none',
+      top: '39%',
+      border: 0
+    }
+  }
+}
+
+const CheckboxStyledDataGrid = styled(DataGrid)(({ theme }) => ({
+  border: 'none',
+  '& .MuiDataGrid-iconSeparator': {
+    display: 'none'
+  },
+  '& .MuiDataGrid-columnHeader, .MuiDataGrid-cell': {
+    border: 'none'
+  },
+  '& .MuiDataGrid-columnsContainer, .MuiDataGrid-cell': {
+    borderBottom: 'none'
+  },
+  ...customCheckbox(theme)
+}))
+
 const FilterMenu = styled((props: MenuProps) => (
   <Menu
     elevation={0}
@@ -116,10 +209,13 @@ export const WorkItemsPage: React.FunctionComponent = () => {
   }
   return (
     <Box padding='1rem'>
+      {/* title */}
       <Typography sx={{ marginLeft: '.5rem', fontSize: '1.125rem', fontWeight: 'light' }}>
         Work items
       </Typography>
+      {/* option bar */}
       <ListOptionsBar />
+      {/* filter bar */}
       <Stack direction='row' sx={{ backgroundColor: grey[100] }}>
         <FilterAltOutlined />
         <Button
@@ -133,7 +229,7 @@ export const WorkItemsPage: React.FunctionComponent = () => {
           onClick={handleClick}
           endIcon={<KeyboardArrowDown />}
         >
-          Options
+          Types
         </Button>
         <FilterMenu
           id="demo-customized-menu"
@@ -175,6 +271,20 @@ export const WorkItemsPage: React.FunctionComponent = () => {
           </Stack>
         </FilterMenu>
       </Stack>
+      {/* table */}
+      <div style={{ height: 400, width: '100%' }}>
+        <CheckboxStyledDataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection={true}
+          disableColumnMenu
+          // components={{
+          //   BaseCheckbox: Check
+          // }}
+        />
+      </div>
     </Box>
   )
 }
