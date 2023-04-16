f6014b702a7cb25c3daf32e9d1eb2d7c1534ab56
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2023-04-16 08:14:30 -0400

    + add Title bar

--- src/components/itemDetailPageTitle/ItemDetailPageTitle.tsx
+++ src/components/itemDetailPageTitle/ItemDetailPageTitle.tsx
@@ -1,25 +1,38 @@
 import { PestControl, ErrorOutlined } from '@mui/icons-material'
-import { Stack, Typography } from '@mui/material'
+import { Grid, Stack, TextField, Typography } from '@mui/material'
 import { red } from '@mui/material/colors'
 import React from 'react'
 
 export const ItemDetailPageTitle: React.FC = () => {
-  return <Stack
-    direction='row'
-    sx={{
-      borderLeftStyle: 'solid',
-      borderLeftWidth: '10px',
-      borderLeftColor: red[900],
-      height: '1.5rem',
-      alignItems: 'center'
-    }}>
-    <PestControl sx={{ marginLeft: '.5rem', color: red[900], fontSize: 'small' }} />
-    <Typography variant='overline'>
-      NEW BUG *
-    </Typography>
-    <ErrorOutlined sx={{ marginLeft: '.5rem', color: red[900], fontSize: 'small' }} />
-    <Typography variant='body2' sx={{ marginLeft: '.25rem', color: red[900] }}>
-      Field &lsquo;Title&lsquo; cannot be empty.
-    </Typography>
-  </Stack>
+  return <Grid>
+    <Stack
+      direction='row'
+      sx={{
+        borderLeftStyle: 'solid',
+        borderLeftWidth: '10px',
+        borderLeftColor: red[900],
+        height: '1.5rem',
+        alignItems: 'center'
+      }}>
+      <PestControl sx={{ marginLeft: '.5rem', color: red[900], fontSize: 'small' }} />
+      <Typography variant='overline'>
+        NEW BUG *
+      </Typography>
+      <ErrorOutlined sx={{ marginLeft: '.5rem', color: red[900], fontSize: 'small' }} />
+      <Typography variant='body2' sx={{ marginLeft: '.25rem', color: red[900] }}>
+        Field &lsquo;Title&lsquo; cannot be empty.
+      </Typography>
+    </Stack>
+    <Stack
+      sx={{
+        borderLeftStyle: 'solid',
+        borderLeftWidth: '10px',
+        borderLeftColor: red[900],
+        height: '3rem',
+        alignItems: 'center'
+      }}
+    >
+      <TextField size='small' sx={{ width: '98%' }} />
+    </Stack>
+  </Grid>
 }
