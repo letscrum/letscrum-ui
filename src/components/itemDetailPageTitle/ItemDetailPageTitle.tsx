75c0dca17b4ad60081b74ad3bd225d802839c1bd
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2023-04-17 07:31:53 -0400

    + add search users UI

--- src/components/itemDetailPageTitle/ItemDetailPageTitle.tsx
+++ src/components/itemDetailPageTitle/ItemDetailPageTitle.tsx
@@ -1,9 +1,12 @@
-import { PestControl, ErrorOutlined } from '@mui/icons-material'
-import { Grid, Stack, TextField, Typography } from '@mui/material'
-import { red } from '@mui/material/colors'
+import { PestControl, ErrorOutlined, AccountCircle, ContactMailOutlined, ClearOutlined } from '@mui/icons-material'
+import { Autocomplete, Avatar, Box, Grid, Stack, TextField, Typography } from '@mui/material'
+import { grey, red } from '@mui/material/colors'
 import React from 'react'
+import { useAppSelector } from '../../redux/hooks'
+import { selectProjectMembers } from '../../redux/reducers/projectSlice'
 
 export const ItemDetailPageTitle: React.FC = () => {
+  const members = useAppSelector(selectProjectMembers)
   return <Grid>
     <Stack
       direction='row'
@@ -28,11 +31,55 @@ export const ItemDetailPageTitle: React.FC = () => {
         borderLeftStyle: 'solid',
         borderLeftWidth: '10px',
         borderLeftColor: red[900],
+        height: '4rem',
+        alignItems: 'center'
+      }}
+    >
+      <TextField size='small' sx={{ marginY: '.5rem', width: '98%' }} />
+    </Stack>
+    <Stack
+      sx={{
+        borderLeftStyle: 'solid',
+        borderLeftWidth: '10px',
+        borderLeftColor: red[900],
         height: '3rem',
         alignItems: 'center'
       }}
     >
-      <TextField size='small' sx={{ width: '98%' }} />
+      <Grid container>
+        <Grid item>
+          <AccountCircle sx={{ marginLeft: '.5rem', color: grey[700] }} />
+        </Grid>
+        <Grid item>
+          <Autocomplete
+            options={members}
+            renderOption={(props, option) => (
+              <Box component='li' {...props}>
+                <Avatar sx={{ width: '2rem', height: '2rem' }}>A</Avatar>
+                <Stack sx={{ marginLeft: '.25rem' }}>
+                  <Typography>
+                    {option.userName}
+                  </Typography>
+                  <Typography>
+                    {option.userId}
+                  </Typography>
+                </Stack>
+                <ClearOutlined sx={{ marginLeft: 'auto', marginRight: '.25rem', fontSize: '1rem', color: red[700] }}/>
+                <ContactMailOutlined sx={{ fontSize: '1rem', color: grey[700] }} />
+              </Box>
+            )}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                inputProps={{
+                  ...params.inputProps
+                }}
+                sx={{ width: '18rem', padding: '0' }}
+              />
+            )}
+          />
+        </Grid>
+      </Grid>
     </Stack>
   </Grid>
 }
