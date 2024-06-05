dc90652535e453404eef3d8bda6eb9139ad70e7c
Wilson Wu <iwilsonwu@gmail.com>
2024-06-05 13:47:52 +0800

    try nextjs with fluentui

---
+++ app/page.tsx
@@ -0,0 +1,85 @@
+"use client" 
+
+import styles from "./page.module.css";
+
+import {
+  Body1,
+  Caption1,
+} from "@fluentui/react-components";
+import { ArrowReplyRegular, ShareRegular } from "@fluentui/react-icons";
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardPreview,
+} from "@fluentui/react-components";
+
+import {
+  Button,
+  Menu,
+  MenuTrigger,
+  MenuList,
+  MenuItem,
+  MenuPopover,
+} from "@fluentui/react-components";
+
+const resolveAsset = (asset: string) => {
+  const ASSET_URL =
+    "https://raw.githubusercontent.com/microsoft/fluentui/master/packages/react-components/react-card/stories/assets/";
+
+  return `${ASSET_URL}${asset}`;
+};
+
+export default function Home() {
+  return (
+    <>
+    <Card className={styles.card}>
+      <CardHeader
+        image={
+          <img
+            src={resolveAsset("avatar_elvia.svg")}
+            alt="Elvia Atkins avatar picture"
+          />
+        }
+        header={
+          <Body1>
+            <b>Elvia Atkins</b> mentioned you
+          </Body1>
+        }
+        description={<Caption1>5h ago · About us - Overview</Caption1>}
+      />
+
+      <CardPreview
+        logo={
+          <img src={resolveAsset("docx.png")} alt="Microsoft Word document" />
+        }
+      >
+        <img
+          src={resolveAsset("doc_template.png")}
+          alt="Preview of a Word document: About Us - Overview"
+        />
+      </CardPreview>
+
+      <CardFooter>
+        <Button icon={<ArrowReplyRegular fontSize={16} />}>Reply</Button>
+        <Button icon={<ShareRegular fontSize={16} />}>Share</Button>
+        
+      </CardFooter>
+    </Card>
+    <Menu>
+    <MenuTrigger disableButtonEnhancement>
+      <Button>Toggle menu</Button>
+    </MenuTrigger>
+
+    <MenuPopover>
+      <MenuList>
+        <MenuItem>New </MenuItem>
+        <MenuItem>New Window</MenuItem>
+        <MenuItem disabled>Open File</MenuItem>
+        <MenuItem>Open Folder</MenuItem>
+      </MenuList>
+    </MenuPopover>
+  </Menu>
+  </>
+  );
+}
--- src/layout/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './signInLayout'
-export * from './mainLayout'
