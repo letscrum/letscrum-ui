6b46fb710123bbad404e7226a25714805c1eb3c7
Wilson Wu <iwilsonwu@gmail.com>
2024-07-12 18:25:30 +0800

    fix switch member and add ex-co-workitems

--- src/pages/sprint/Taskboard.vue
+++ src/pages/sprint/Taskboard.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-
+    <v-btn @click="AddWorkItem()"></v-btn>
     <v-row no-gutters>
       <v-col>
         <v-data-iterator
           :items="workItems"
           hide-default-footer
+          v-model:expanded="expanded"
+          items-per-page="-1"
         >
           <template v-slot:header>
             <v-row no-gutters>
               <v-col cols="2">
-                <!-- <v-btn variant="plain" prepend-icon="mdi-arrow-expand-vertical" size="x-small" @click="expandAll = !expandAll" v-if="expandAll">
+                <v-btn variant="plain" prepend-icon="mdi-arrow-expand-vertical" size="x-small" @click="expanded = []" v-if="expanded.length > 0">
                   Expand all
                 </v-btn>
-                <v-btn variant="plain" prepend-icon="mdi-arrow-collapse-vertical" size="x-small" @click="expandAll = !expandAll" v-else>
+                <v-btn variant="plain" prepend-icon="mdi-arrow-collapse-vertical" size="x-small" @click="collapseAll()" v-else>
                   Collapse all
-                </v-btn> -->
+                </v-btn>
               </v-col>
               <v-col cols="10">
                 <v-row no-gutters>
@@ -34,7 +36,7 @@
           </template>
           <template v-slot:default="{ items, isExpanded, toggleExpand }">
             <div v-for="item in items" :key="item.raw.id">
-              <v-expand-transition>
+              <v-expand-transition v-if="item.raw.id > 0">
                 <v-row no-gutters v-if="!isExpanded(item)">
                   <v-col cols="2">
                     <v-icon icon="mdi-menu-down" size="x-small" width="10%" class="float-left" @click="() => toggleExpand(item)"></v-icon>
@@ -99,6 +101,20 @@
                   </v-col>
                 </v-row>
               </v-expand-transition>
+              <div v-else>
+                <v-row no-gutters v-if="!isExpanded(item)">
+                  <v-col cols="2">
+                    <v-icon icon="mdi-menu-down" size="x-small" width="10%" class="float-left"></v-icon>
+                    <v-sheet tile outlined class="mb-2 mr-2 float-left"
+                      style="border-left-color: rgb(0, 156, 204); border-width: 1px; border-left-width: 3px;"
+                      width="85%" min-width="160" height="100">
+                        <input style="border-width: 1px; border-style: solid; border-color: gray; margin: 2px 2px 2px 2px;" type="textarea" />
+                    </v-sheet>
+                  </v-col>
+                  <v-col cols="10">
+                  </v-col>
+                </v-row>
+              </div>
             </div>
           </template>
         </v-data-iterator>
@@ -143,7 +159,7 @@ const workItems = ref([])
 const allWorkItems = ref([])
 
 const workDetail = ref(false)
-const expandAll = ref(false)
+const expanded = ref([])
 
 function LoadWorkItems() {
   getGetWorkItems(route.params.projectId, {
@@ -151,27 +167,65 @@ function LoadWorkItems() {
   }).then(res => {
     allWorkItems.value = res.data.items
     workItems.value = res.data.items
-    console.log(workItems.value)
+    console.log('workItems', workItems.value)
   })
 }
 
+function AddWorkItem() {
+  console.log('expanded', expanded.value)
+  // remove last item from expanded list
+  expanded.value.pop()
+}
 
 function filterTasks(userId) {
-  console.log("userId", userId)
-  console.log(allWorkItems.value)
+  console.log('userId', userId, typeof userId)
+  let myItems = []
   if (userId > 0) {
-    workItems.value = allWorkItems.value.filter(item => item.assignUser.id === userId)
+    // if allWorkItems assignUser.id or any tasks assignUser.id is equal to userId, add to myItems
+    myItems = allWorkItems.value.filter(item => {
+      if (item.assignUser.id === userId.toString()) {
+        return item
+      }
+      for (let i = 0; i < item.tasks.length; i++) {
+        if (item.tasks[i].assignUser.id === userId.toString()) {
+          return item
+        }
+      }
+    })
   }
   if (userId === 0) {
-    workItems.value = allWorkItems.value.filter(item => item.assignUser.id === Number(store.user.id))
+    myItems = allWorkItems.value.filter(item => {
+      if (item.assignUser.id === store.user.id.toString()) {
+        return item
+      }
+      for (let i = 0; i < item.tasks.length; i++) {
+        if (item.tasks[i].assignUser.id === store.user.id.toString()) {
+          return item
+        }
+      }
+    })
   }
   if (userId === -1) {
-    workItems.value = allWorkItems.value
+    myItems = allWorkItems.value
+  }
+  expanded.value = []
+  // if in allWorkItems, but not in myItems, add to expanded
+  for (let i = 0; i < allWorkItems.value.length; i++) {
+    if (!myItems.includes(allWorkItems.value[i])) {
+      expanded.value.push((i+1).toString())
+    }
   }
 }
 
 defineExpose({ filterTasks })
 
+function collapseAll() {
+  expanded.value = []
+  for (let i = 0; i < workItems.value.length; i++) {
+    expanded.value.push((i+1).toString())
+  }
+  console.log('expanded', expanded.value)
+}
 
 
 onMounted(() => {
