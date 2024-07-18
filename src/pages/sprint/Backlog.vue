cfc1475c63ae009f299ef96c3c302466aaaaa1b4
Wilson Wu <iwilsonwu@gmail.com>
2024-07-18 19:14:21 +0800

    add pages for sprint

--- src/pages/sprint/Backlog.vue
+++ src/pages/sprint/Backlog.vue
@@ -3,20 +3,51 @@
     <v-data-table
       :headers="headers"
       :items="workItems"
-      :expanded="expanded"
+      v-model:expanded="expanded"
       item-key="id"
+      show-expand
     >
-      <template v-slot:expanded-item="{ headers, item }">
-        <td :colspan="headers.length">
-          More info about {{ item.name }}
-        </td>
+      <template v-slot:headers="{ columns, isSorted, getSortIcon, toggleSort }">
+        <tr>
+          <th
+            v-for="column in columns"
+            :key="column.text"
+            @click="toggleSort(column)"
+            :class="[
+              'column sortable',
+              isSorted(column) ? 'sorted' : '',
+              column.align === 'start' ? 'text-start' : '',
+            ]"
+          >
+            <v-icon small>{{ getSortIcon(column) }}</v-icon>
+            {{ column.text }}
+          </th>
+        </tr>
+      </template>
+
+      <template v-slot:item="{ item }">
+        <tr>
+          <td>{{ item.order }}</td>
+          <td>{{ item.id }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.assign }}</td>
+          <td>{{ item.status }}</td>
+          <td>{{ item.type }}</td>
+        </tr>
+      </template>
+      <template v-slot:expanded-row="{ columns, item }">
+        <tr>
+          <td :colspan="columns.length">
+            More info about {{ item.name }}
+          </td>
+        </tr>
       </template>
     </v-data-table>
   </div>
 </template>
 <script setup>
 
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { getGetWorkItems } from '@/apis/workitem'
 import { useAppStore } from '@/stores/app'
@@ -27,7 +58,7 @@ const route = useRoute()
 
 const workItems = ref([])
 const expanded = ref([])
-const headers = [
+const headers = ref([
   {
     text: 'Order',
     align: 'start',
@@ -39,7 +70,8 @@ const headers = [
   { text: 'Assign', value: 'assign' },
   { text: 'Status', value: 'status' },
   { text: 'Type', value: 'type' },
-]
+  { title: '', key: 'data-table-expand' },
+])
 
 
 function LoadWorkItems() {
--- src/pages/sprint/Capacity.vue
+++ src/pages/sprint/Capacity.vue
@@ -1,124 +1,39 @@
 <template>
   <div>
-    <v-row no-gutters>
-      <v-col>
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">
-                  Name
-                </th>
-                <th class="text-left">
-                  Role
-                </th>
-                <th class="text-left">
-                  Capacity per day
-                </th>
-                <th>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="member in sprintMembers"
-                :key="member.name"
-              >
-                <td>
-                  <v-list-item>
-                    <v-list-item-avatar color="red" size="30">
-                      <span class="white--text text-h6">
-                        {{ member.userName.substring(0, 1).toUpperCase() }}
-                      </span>
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                      <v-list-item-title>{{ member.userName }}</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </td>
-                <td>
-                  <v-select
-                    class="mt-0 pt-0"
-                    :items="roles"
-                    hide-details
-                    dense
-                    style="width: 180px;"
-                  ></v-select>
-                </td>
-                <td>
-                  <v-text-field
-                    :value="member.capacity"
-                    class="mt-0 pt-0"
-                    hide-details
-                    single-line
-                    dense
-                    type="number"
-                    style="width: 120px;"
-                  ></v-text-field>
-                </td>
-                <td>
-                  <v-btn text icon>
-                    <v-icon>
-                      mdi-close
-                    </v-icon>
-                  </v-btn>
-                </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-col>
-      <v-col v-if="workDetail" cols="3">
-        <v-card flat tile>
-          <v-card-title>
-            <span class="text-h5">Work Details</span>
-            <v-spacer></v-spacer>
-            <v-btn icon @click="workDetail = false">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </v-card-title>
-          <v-card-text>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-data-table
+      :items="members"
+    >
+    </v-data-table>
   </div>
 </template>
 
-<script>
-import { getGetSprintMembers } from '@/apis/sprint';
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { getGetSprint } from '@/apis/sprint'
 
-export default {
-  name: 'SprintCapacity',
-  data: () => ({
-    sprintMembers: [],
-    roles: ['Development', 'Testing', 'Documentation'],
-    workDetail: false
-  }),
-  created() {
-    this.onLoadSprintMembers();
-  },
-  methods: {
-    onLoadSprintMembers() {
-      getGetSprintMembers(this.project.id, this.sprint.id, {
-        page: 1,
-        size: 999
-      }).then((res) => {
-        console.log(res);
-        if (res.status === 200) {
-          this.sprintMembers = res.data.items;
-        }
-      });
-    },
-    onSetRole(userId, role) {
-      this.sprintMembers.forEach((m) => {
-        if (m.userId === userId) {
-          // eslint-disable-next-line no-param-reassign
-          m.role = role;
-        }
-      });
-      console.log(this.sprintMembers);
+
+const route = useRoute()
+const sprint = ref({})
+const members = ref([])
+const headers = ref([
+  { text: 'Order', value: 'order' },
+  { text: 'ID', value: 'id' },
+  { text: 'Title', value: 'title' },
+  { text: 'Assign', value: 'assign' },
+  { text: 'Status', value: 'status' },
+])
+
+function LoadSprint() {
+  getGetSprint(route.params.projectId, route.params.sprintId).then((res) => {
+    if (res.status === 200) {
+      sprint.value = res.data.item
+      members.value = res.data.item.members
     }
-  }
-};
+  })
+}
+
+onMounted(() => {
+  LoadSprint()
+})
 </script>
