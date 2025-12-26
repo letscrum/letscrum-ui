b2c2ddd3871c464f2f6349a3164b8f8cb0dd1ac1
Wilson Wu <iwilsonwu@gmail.com>
2025-12-26 18:06:08 +0800

    feat: enhance project creation UI with improved layout, validation, and localization support

--- src/components/project/ProjectCreate.vue
+++ src/components/project/ProjectCreate.vue
@@ -1,7 +1,7 @@
 <template>
   <v-dialog
     v-model="dialog"
-    width="50%"
+    width="600"
     persistent
   >
     <template #activator="{ props: activatorProps }">
@@ -11,86 +11,123 @@
     </template>
 
     <template #default="{ isActive }">
-      <v-card
-        prepend-icon="mdi-earth"
-        title="Select Country"
-      >
-        <v-divider class="my-1"></v-divider>
+      <v-card rounded="lg" elevation="2">
+        <v-card-title class="d-flex align-center pa-4">
+          <v-icon icon="mdi-folder-plus" class="mr-2" color="primary"></v-icon>
+          {{ $t('project.create.title') }}
+        </v-card-title>
 
-        <v-card-text class="px-4">
-          <v-text-field v-model="project.displayName" label="Label"></v-text-field>
+        <v-divider></v-divider>
 
-          <v-textarea v-model="project.description" label="Label"></v-textarea>
+        <v-card-text class="pa-4">
+          <v-form ref="form" @submit.prevent="createProject">
+            <v-text-field
+              v-model="project.name"
+              :label="$t('project.create.name')"
+              variant="outlined"
+              density="compact"
+              class="mb-2"
+              required
+            ></v-text-field>
 
-          <v-autocomplete
-            v-model:search-input="search"
-            v-model="admins"
-            chips
-            :items="users"
-            label="Autocomplete"
-            multiple
-            item-props
-            no-filter
-            @update:search="searchUsers"
-          >
-            <template #chip="{ props, item }">
-              <v-chip
-:closable="!item.raw.owner"
-                v-bind="props"
-                :text="item.raw.name"
-              ></v-chip>
-            </template>
-            <template #item="{ props, item }">
-              <v-list-item
-                v-bind="props"
-                :title="item.raw.name"
-              ></v-list-item>
-            </template>
-          </v-autocomplete>
+            <v-text-field
+              v-model="project.displayName"
+              :label="$t('project.create.displayName')"
+              variant="outlined"
+              density="compact"
+              class="mb-2"
+            ></v-text-field>
 
-          <v-autocomplete
-            v-model:search-input="search"
-            v-model="members"
-            chips
-            :items="users"
-            label="Autocomplete"
-            multiple
-            item-props
-            no-filter
-            @update:search="searchUsers"
-          >
-            <template #chip="{ props, item }">
-              <v-chip
-:closable="!item.raw.owner"
-                v-bind="props"
-                :text="item.raw.name"
-              ></v-chip>
-            </template>
-            <template #item="{ props, item }">
-              <v-list-item
-                v-bind="props"
-                :title="item.raw.name"
-              ></v-list-item>
-            </template>
-          </v-autocomplete>
+            <v-textarea
+              v-model="project.description"
+              :label="$t('project.create.description')"
+              variant="outlined"
+              density="compact"
+              rows="3"
+              auto-grow
+              class="mb-2"
+            ></v-textarea>
+
+            <v-autocomplete
+              v-model:search-input="search"
+              v-model="admins"
+              chips
+              :items="users"
+              :label="$t('project.create.admins')"
+              multiple
+              item-props
+              no-filter
+              variant="outlined"
+              density="compact"
+              class="mb-2"
+              @update:search="searchUsers"
+            >
+              <template #chip="{ props, item }">
+                <v-chip
+                  :closable="!item.raw.owner"
+                  v-bind="props"
+                  :text="item.raw.name"
+                  size="small"
+                ></v-chip>
+              </template>
+              <template #item="{ props, item }">
+                <v-list-item
+                  v-bind="props"
+                  :title="item.raw.name"
+                ></v-list-item>
+              </template>
+            </v-autocomplete>
+
+            <v-autocomplete
+              v-model:search-input="search"
+              v-model="members"
+              chips
+              :items="users"
+              :label="$t('project.create.members')"
+              multiple
+              item-props
+              no-filter
+              variant="outlined"
+              density="compact"
+              @update:search="searchUsers"
+            >
+              <template #chip="{ props, item }">
+                <v-chip
+                  :closable="!item.raw.owner"
+                  v-bind="props"
+                  :text="item.raw.name"
+                  size="small"
+                ></v-chip>
+              </template>
+              <template #item="{ props, item }">
+                <v-list-item
+                  v-bind="props"
+                  :title="item.raw.name"
+                ></v-list-item>
+              </template>
+            </v-autocomplete>
+          </v-form>
         </v-card-text>
 
         <v-divider></v-divider>
 
-        <v-card-actions>
-          <v-btn
-            text="Close"
-            @click="isActive.value = false"
-          ></v-btn>
-
+        <v-card-actions class="pa-4">
           <v-spacer></v-spacer>
+          <v-btn
+            variant="text"
+            @click="isActive.value = false"
+          >
+            {{ $t('project.create.cancel') }}
+          </v-btn>
 
           <v-btn
-            color="surface-variant"
-            text="Save"
+            color="primary"
             variant="flat"
+            :loading="loading"
             @click="createProject()"
-          ></v-btn>
+          >
+            {{ $t('project.create.confirm') }}
+          </v-btn>
         </v-card-actions>
       </v-card>
     </template>
@@ -98,14 +135,80 @@
 </template>
 
 <script setup>
-
 import { ref } from 'vue';
-
-import { postCreateOrg, getGetOrgs } from '@/apis/org';
+import { postCreateProject } from '@/apis/project';
+import { getGetOrgMembers } from '@/apis/org';
 import { useAppStore } from '@/stores/app';
 
-const project = ref({})
-const dialog = ref(false)
-const store = useAppStore()
+const emit = defineEmits(['after-create']);
+const store = useAppStore();
 
+const project = ref({});
+const admins = ref([]);
+const members = ref([]);
+const users = ref([]);
+const search = ref('');
+const dialog = ref(false);
+const loading = ref(false);
+
+const searchUsers = val => {
+  if (!val) {
+    users.value = [];
+    return false;
+  }
+  getGetOrgMembers(store.org.id).then((res) => {
+    if (res.status === 200) {
+      users.value = res.data.items.filter((user) => user.member.name.includes(val)).map((item) => {
+        return {
+          id: item.member.id,
+          name: item.member.name,
+          isAdmin: item.isAdmin,
+          owner: item.member.id === store.user.id ? true : false
+        }
+      });
+    }
+  });
+}
+
+function onOpenCreate() {
+  project.value = {};
+  admins.value = [{
+    id: store.user.id,
+    name: store.user.name,
+    isAdmin: true,
+    owner: true
+  }];
+  members.value = [];
+  dialog.value = true;
+}
+
+function createProject() {
+  if (!project.value.name) return;
+
+  loading.value = true;
+
+  // Prepare members list
+  const adminList = admins.value.filter(admin => !admin.owner).map(admin => ({ ...admin, isAdmin: true }));
+  const memberList = members.value.map(member => ({ ...member, isAdmin: false }));
+  const allMembers = [...adminList, ...memberList];
+
+  postCreateProject(store.org.id, {
+    name: project.value.name,
+    displayName: project.value.displayName,
+    description: project.value.description,
+    members: allMembers.map((member) => ({
+      userId: member.id,
+      userName: member.name,
+      isAdmin: member.isAdmin,
+    }))
+  }).then((res) => {
+    if (res.status === 200) {
+      emit('after-create');
+      dialog.value = false;
+    }
+    loading.value = false;
+  }).catch(() => {
+    loading.value = false;
+  });
+}
 </script>
--- src/pages/project/ProjectList.vue
+++ src/pages/project/ProjectList.vue
@@ -9,111 +9,16 @@
           </p>
         </v-col>
         <v-col cols="12" sm="4" class="text-sm-right">
-          <v-dialog
-            v-model="dialog"
-            width="50%"
-            persistent
-          >
-            <template #activator="{ props: activatorProps }">
-              <v-btn
-                color="primary"
-                prepend-icon="mdi-plus"
-                elevation="2"
-                size="large"
-                v-bind="activatorProps"
-                @click="onOpenCreate()"
-              >
-                {{ $t('project.list.createBtn') }}
-              </v-btn>
-            </template>
-
-            <template #default="{ isActive }">
-              <v-card
-                prepend-icon="mdi-earth"
-                title="Select Country"
-              >
-                <v-divider class="my-1"></v-divider>
-
-                <v-card-text class="px-4">
-                  <v-text-field v-model="project.name" label="Label"></v-text-field>
-
-                  <v-text-field v-model="project.displayName" label="Label"></v-text-field>
-
-                  <v-textarea v-model="project.description" label="Label"></v-textarea>
-
-                  <v-autocomplete
-                    v-model:search-input="search"
-                    v-model="admins"
-                    chips
-                    :items="users"
-                    label="Autocomplete"
-                    multiple
-                    item-props
-                    no-filter
-                    @update:search="searchUsers"
-                  >
-                    <template #chip="{ props, item }">
-                      <v-chip
-                        :closable="!item.raw.owner"
-                        v-bind="props"
-                        :text="item.raw.name"
-                      ></v-chip>
-                    </template>
-                    <template #item="{ props, item }">
-                      <v-list-item
-                        v-bind="props"
-                        :title="item.raw.name"
-                      ></v-list-item>
-                    </template>
-                  </v-autocomplete>
-
-                  <v-autocomplete
-                    v-model:search-input="search"
-                    v-model="members"
-                    chips
-                    :items="users"
-                    label="Autocomplete"
-                    multiple
-                    item-props
-                    no-filter
-                    @update:search="searchUsers"
-                  >
-                    <template #chip="{ props, item }">
-                      <v-chip
-                        :closable="!item.raw.owner"
-                        v-bind="props"
-                        :text="item.raw.name"
-                      ></v-chip>
-                    </template>
-                    <template #item="{ props, item }">
-                      <v-list-item
-                        v-bind="props"
-                        :title="item.raw.name"
-                      ></v-list-item>
-                    </template>
-                  </v-autocomplete>
-                </v-card-text>
-
-                <v-divider></v-divider>
-
-                <v-card-actions>
-                  <v-btn
-                    text="Close"
-                    @click="isActive.value = false"
-                  ></v-btn>
-
-                  <v-spacer></v-spacer>
-
-                  <v-btn
-                    color="surface-variant"
-                    text="Save"
-                    variant="flat"
-                    @click="createProject()"
-                  ></v-btn>
-                </v-card-actions>
-              </v-card>
-            </template>
-          </v-dialog>
+          <ProjectCreate @after-create="LoadProjects">
+            <v-btn
+              color="primary"
+              prepend-icon="mdi-plus"
+              elevation="2"
+              size="large"
+            >
+              {{ $t('project.list.createBtn') }}
+            </v-btn>
+          </ProjectCreate>
         </v-col>
       </v-row>
 
@@ -160,99 +65,20 @@
 
 <script setup>
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import ProjectCard from '@/components/project/ProjectCard.vue'
+import ProjectCreate from '@/components/project/ProjectCreate.vue'
 
 import { useAppStore } from '@/stores/app'
 import { onMounted, ref } from 'vue';
 
 const store = useAppStore()
 
-import { getGetProjects, postCreateProject } from '@/apis/project'
-import { getGetOrgMembers } from '@/apis/org'
+import { getGetProjects } from '@/apis/project'
 import { onBeforeRouteUpdate } from 'vue-router';
 
 const projects = ref([])
-const project = ref({})
-const admins = ref([])
-const members = ref([])
-const users = ref([])
-const search = ref()
-const dialog = ref(false)
 const loading = ref(true)
 
-
-const searchUsers = val => {
-  if (!val) {
-    users.value = []
-    return false
-  }
-  getGetOrgMembers(store.org.id).then((res) => {
-    console.log(res)
-    if (res.status === 200) {
-      users.value = res.data.items.filter((user) => user.member.name.includes(val)).map((item) => {
-        return {
-          id: item.member.id,
-          name: item.member.name,
-          isAdmin: item.isAdmin,
-          owner: item.member.id === store.user.id ? true : false
-        }
-      })
-      console.log(members.value)
-    }
-  })
-}
-
-
-function onOpenCreate() {
-  project.value = {}
-  admins.value = [{
-    id: store.user.id,
-    name: store.user.name,
-    isAdmin: true,
-    owner: true
-  }]
-  members.value = []
-  dialog.value = true
-}
-
-function createProject() {
-  console.log(project.value)
-  console.log(admins.value)
-  console.log(members.value)
-  // set all isAdmin to true in admins
-  admins.value = admins.value.map((admin) => {
-    admin.isAdmin = true
-    return admin
-  })
-  // set all isAdmin to false in members
-  members.value = members.value.map((member) => {
-    member.isAdmin = false
-    return member
-  })
-  // remove the owner from the admins
-  admins.value = admins.value.filter((admin) => {
-    return !admin.owner
-  })
-  // combine the admins and members
-  let allMembers = admins.value.concat(members.value)
-
-  postCreateProject(store.org.id, {
-    name: project.value.name,
-    description: project.value.description,
-    members: allMembers.map((member) => {
-      return {
-        userId: member.id,
-        userName: member.name,
-        isAdmin: member.isAdmin,
-      }
-    })
-  }).then((res) => {
-    console.log(res)
-    if (res.status === 200) {
-      LoadProjects()
-      dialog.value = false
-    }
-  })
-}
 
 onMounted(() => {
   LoadProjects()
