d9b63fda6da2b66113e1714775af384c348adf38
Wilson Wu <iwilsonwu@gmail.com>
2025-12-30 18:00:24 +0800

    feat: enhance sprint capacity table with improved layout and styling

--- src/pages/sprint/Capacity.vue
+++ src/pages/sprint/Capacity.vue
@@ -1,53 +1,64 @@
 <template>
-  <div>
-    <v-table>
-      <thead>
-        <tr>
-          <th class="text-left">
-          </th>
-          <th class="text-left">
-            Name
-          </th>
-          <th class="text-left">
-            Role
-          </th>
-          <th class="text-left">
-            Capacity
-          </th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in currentMembers"
-          :key="item.userId"
-        >
-          <td>
-            <user-avatar :user-id="item.userId" :user-name="item.userName"></user-avatar>
-          </td>
-          <td>{{ item.userName }}</td>
-          <td>
-            <select v-model="item.role">
-              <option value="Unassigned">Unassigned</option>
-              <option value="Development">Development</option>
-              <option value="Designer">Designer</option>
-              <option value="Testing">Testing</option>
-            </select>
-          </td>
-          <td>
-            <input v-model="item.capacity" />
-          </td>
-          <td>
-            <SprintMemberDelete :member="item" @after="LoadSprint">
-              <v-btn>
-                X
-              </v-btn>
-            </SprintMemberDelete>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
-  </div>
+  <v-container fluid class="pa-4">
+    <v-card class="elevation-1">
+      <v-card-title class="text-h6 font-weight-bold pa-4">
+        Sprint Capacity Planning
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-text class="pa-0">
+        <v-table hover>
+          <thead>
+            <tr class="bg-grey-lighten-4">
+              <th class="text-center" style="width: 80px">Avatar</th>
+              <th class="text-left">Name</th>
+              <th class="text-left" style="width: 250px">Role</th>
+              <th class="text-left" style="width: 150px">Capacity (Hrs)</th>
+              <th class="text-center" style="width: 80px">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in currentMembers" :key="item.userId">
+              <td class="text-center py-2">
+                <user-avatar :user-id="item.userId" :user-name="item.userName"></user-avatar>
+              </td>
+              <td class="font-weight-medium">{{ item.userName }}</td>
+              <td class="py-2">
+                <v-select
+                  v-model="item.role"
+                  :items="['Unassigned', 'Development', 'Designer', 'Testing']"
+                  variant="outlined"
+                  density="compact"
+                  hide-details
+                  bg-color="white"
+                ></v-select>
+              </td>
+              <td class="py-2">
+                <v-text-field
+                  v-model.number="item.capacity"
+                  type="number"
+                  variant="outlined"
+                  density="compact"
+                  hide-details
+                  bg-color="white"
+                  min="0"
+                ></v-text-field>
+              </td>
+              <td class="text-center">
+                <SprintMemberDelete :member="item" @after="LoadSprint">
+                  <v-btn icon="mdi-delete" color="error" variant="text" size="small"></v-btn>
+                </SprintMemberDelete>
+              </td>
+            </tr>
+            <tr v-if="currentMembers.length === 0">
+              <td colspan="5" class="text-center py-4 text-grey">
+                No members in this sprint. Add members from the menu.
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script setup>
